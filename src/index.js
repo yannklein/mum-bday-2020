d4832e290c90b30993cc2ba2d8566cdae41b6ac2
Yann Klein <yann.klein@me.com>
2020-01-16 14:19:03 +0900

    styling adjustemen ts

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ stories.forEach( (story) => {
     // console.log(story);
     const storyPosition = story.offsetTop - window.scrollY;
     // If the story is 10px from the top, we display it
-    if (storyPosition < 200 && !story.classList.contains("opacity-1")) {
+    if (storyPosition < 250 && !story.classList.contains("opacity-1")) {
       story.classList.add("opacity-1");
     } 
   });
